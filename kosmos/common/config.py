359f00523ef78ec617e279d1076600aa0eccbe70
Doug Wiegley <dougw@a10networks.com>
2015-12-09 14:58:43 -0700

    A few common setup routines and packages
    Change-Id: I4b2a3f1c0c124dfb4e335cbcdb160a01ad1c54a3

---
+++ kosmos/common/config.py
@@ -0,0 +1,74 @@
+# Copyright 2011 VMware, Inc., 2014 A10 Networks
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Routines for configuring Kosmos
+"""
+
+from oslo_config import cfg
+from oslo_db import options as db_options
+from oslo_log import log as logging
+import oslo_messaging as messaging
+
+from kosmos._i18n import _LI
+from kosmos.common import utils
+from kosmos import version
+
+LOG = logging.getLogger(__name__)
+
+core_opts = [
+    cfg.IPOpt('bind_host', default='0.0.0.0',
+              help=_("The host IP to bind to")),
+    cfg.PortOpt('bind_port', default=9876,
+                help=_("The port to bind to")),
+    cfg.StrOpt('host', default=utils.get_hostname(),
+               help=_("The hostname Kosmos is running on")),
+]
+
+core_cli_opts = []
+
+
+# Register the configuration options
+cfg.CONF.register_opts(core_opts)
+cfg.CONF.register_cli_opts(core_cli_opts)
+
+
+# Ensure that the control exchange is set correctly
+messaging.set_transport_defaults(control_exchange='kosmos')
+_SQL_CONNECTION_DEFAULT = 'sqlite://'
+# Update the default QueuePool parameters. These can be tweaked by the
+# configuration variables - max_pool_size, max_overflow and pool_timeout
+db_options.set_defaults(cfg.CONF,
+                        connection=_SQL_CONNECTION_DEFAULT,
+                        sqlite_db='', max_pool_size=10,
+                        max_overflow=20, pool_timeout=10)
+
+logging.register_options(cfg.CONF)
+
+
+def init(args, **kwargs):
+    cfg.CONF(args=args, project='kosmos',
+             version='%%prog %s' % version.version_info.release_string(),
+             **kwargs)
+
+
+def setup_logging(conf):
+    """Sets up the logging options for a log with supplied name.
+
+    :param conf: a cfg.ConfOpts object
+    """
+    product_name = "kosmos"
+    logging.setup(conf, product_name)
+    LOG.info(_LI("Logging enabled!"))
