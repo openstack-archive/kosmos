2e38b96f9da66a4a458c56bee8f39cd72890fb98
Graham Hayes <graham.hayes@hpe.com>
2015-12-15 16:46:40 +0000

    Add Basic utils / requirements
    Change-Id: Ifb921c92223454c9d0831293c8fe13dd321edc03

---
+++ kosmos/api/__init__.py
@@ -0,0 +1,29 @@
+# Copyright 2015 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from oslo_config import cfg
+
+cfg.CONF.register_group(cfg.OptGroup(
+    name='service:api', title="Configuration for Engine Service"
+))
+
+cfg.CONF.register_opts([
+    cfg.IntOpt('workers', default=3,
+               help='Number of central worker processes to spawn'),
+    cfg.IntOpt('threads', default=1000,
+               help='Number of central greenthreads to spawn'),
+    cfg.IPOpt('bind_host', default="0.0.0.0",
+              help='IP address for the API to listen to'),
+    cfg.PortOpt('bind_port', default=9100,
+                help='Port for the API to listen to')
+], group='service:api')
