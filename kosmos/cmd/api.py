8baec771054027577b91cfca94703b3ab03fb45e
Graham Hayes <graham.hayes@hpe.com>
2015-12-15 16:46:40 +0000

    Add Basic utils / requirements
    Change-Id: Ifb921c92223454c9d0831293c8fe13dd321edc03

---
+++ kosmos/cmd/api.py
@@ -0,0 +1,43 @@
+# Copyright 2015 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import sys
+
+from oslo_config import cfg
+from oslo_service import service
+from oslo_service.wsgi import Loader
+
+from kosmos.api import service as api
+from kosmos.common import config
+
+CONF = cfg.CONF
+
+
+def main():
+
+    config.setup_logging(CONF)
+    config.init(sys.argv)
+
+    process_launcher = service.ProcessLauncher(CONF)
+    process_launcher.launch_service(
+        api.Service(
+            CONF,
+            'API',
+            Loader(CONF).load_app('kosmos'),
+            host=CONF['service:api'].bind_host,
+            port=CONF['service:api'].bind_port
+        ),
+
+        workers=CONF['service:api'].workers)
+
+    process_launcher.wait()
--- kosmos/conductor/service.py
+++ kosmos/conductor/service.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-
+# Copyright 2015 Hewlett Packard Enterprise Development LP
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -11,23 +11,27 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import os
-
-import pbr.version
 from oslo_config import cfg
+import oslo_messaging as messaging
+from oslo_log import log as logging
+
+from kosmos import service
 
 
-__version__ = pbr.version.VersionInfo(
-    'kosmos').version_string()
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
 
 
-cfg.CONF.register_opts([
-    cfg.StrOpt(
-        'pybasedir',
-        default=os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                             '../')),
-        help='Directory where the kosmos python module is installed'
-    ),
-    cfg.StrOpt('state-path', default='/var/lib/kosmos',
-               help='Top-level directory for maintaining kosmos\'s state'),
-])
+class Service(service.RPCService, service.Service):
+    """
+    API version history:
+
+        1.0 - Initial version
+    """
+    RPC_API_VERSION = '1.0'
+
+    target = messaging.Target(version=RPC_API_VERSION)
+
+    @property
+    def service_name(self):
+        return 'conductor'
