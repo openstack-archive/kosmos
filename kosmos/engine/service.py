2f61555b766b75809bc5a932e5eb2ce8f02ac96c
Graham Hayes <graham.hayes@hpe.com>
2015-12-15 16:46:40 +0000

    Add Basic utils / requirements
    Change-Id: Ifb921c92223454c9d0831293c8fe13dd321edc03

--- kosmos/engine/service.py
+++ kosmos/engine/service.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Hewlett Packard Enterprise Development LP
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -12,8 +11,27 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+from oslo_config import cfg
+import oslo_messaging as messaging
+from oslo_log import log as logging
 
-import pbr.version
+from kosmos import service
 
-__version__ = pbr.version.VersionInfo(
-    'kosmos').version_string()
+
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+
+
+class Service(service.RPCService, service.Service):
+    """
+    API version history:
+
+        1.0 - Initial version
+    """
+    RPC_API_VERSION = '1.0'
+
+    target = messaging.Target(version=RPC_API_VERSION)
+
+    @property
+    def service_name(self):
+        return 'engine'
